7fc9ea1ed0a715bf0332ad11d5e454b13ba42ded
Boyan <boiansinilkov@abv.bg>
2022-07-02 01:06:38 +0300

    -added a way to read directories form a location

---
+++ Bomb/src/com/reading_directories.java
@@ -0,0 +1,25 @@
+
+package com;
+
+import java.io.File;
+
+public class reading_directories {
+    public static void main(String[] args) {
+        // Creates an array in which we will store the names of files and directories
+        String[] pathnames;
+
+        // Creates a new File instance by converting the given pathname string
+        // into an abstract pathname
+        File f = new File("G:");
+
+        // Populates the array with names of files and directories
+        pathnames = f.list();
+
+        // For each pathname in the pathnames array
+        for (String pathname : pathnames) {
+            // Print the names of files and directories
+            System.out.println(pathname);
+        }
+    }
+
+}
